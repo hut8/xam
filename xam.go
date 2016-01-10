7808a71081d0efe77e7bd8e2e43152be89f240d7
Liam Bowen <liambowen@gmail.com>
2016-01-09 20:18:37 -0500

    Turn into library instead of CLI

--- xam.go
+++ xam.go
@@ -1,16 +1,11 @@
-package main
+package xam
 
 import (
 	"crypto/sha1"
-	"encoding/csv"
 	"encoding/hex"
 	"io"
 	"os"
 	"path/filepath"
-	"runtime"
-	"strconv"
-
-	"github.com/bradfitz/iter"
 )
 
 // FileData represents attributes of file
@@ -21,9 +16,9 @@ type FileData struct {
 	Err  error
 }
 
-// walkFSTree passes each file encountered while walking tree into fileDataChan
+// WalkFSTree passes each file encountered while walking tree into fileDataChan
 // rootPath should be an absolute path
-func walkFSTree(fileDataChan chan FileData, rootPath string) {
+func WalkFSTree(fileDataChan chan FileData, rootPath string) {
 	filepath.Walk(
 		rootPath,
 		func(path string, info os.FileInfo, err error) error {
@@ -35,7 +30,7 @@ func walkFSTree(fileDataChan chan FileData, rootPath string) {
 		})
 }
 
-func hashFile(path string) ([]byte, error) {
+func HashFile(path string) ([]byte, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -50,7 +45,7 @@ func hashFile(path string) ([]byte, error) {
 	return hasher.Sum(nil), nil
 }
 
-func hashFileHex(path string) (string, error) {
+func HashFileHex(path string) (string, error) {
 	h, err := hashFile(path)
 	if err != nil {
 		return "", err
@@ -58,58 +53,11 @@ func hashFileHex(path string) (string, error) {
 	return hex.EncodeToString(h), nil
 }
 
-func computeHashes(output chan FileData, input chan FileData) {
+func ComputeHashes(output chan FileData, input chan FileData) {
 	for f := range input {
 		h, err := hashFileHex(f.Path)
 		f.SHA1 = h
 		f.Err = err
 		output <- f
 	}
-}
-
-func writeCSV(fileData chan FileData, csvFile io.Writer) {
-	w := csv.NewWriter(csvFile)
-	w.Write([]string{
-		"path", "modified", "size", "mode", "sha1", "error",
-	})
-
-	for d := range fileData {
-		errorStr := ""
-		if d.Err != nil {
-			errorStr = d.Err.Error()
-		}
-		w.Write([]string{
-			d.Path,
-			d.ModTime().String(),
-			strconv.FormatInt(d.Size(), 10),
-			d.Mode().String(),
-			d.SHA1,
-			errorStr,
-		})
-	}
-}
-
-func main() {
-	root := os.Getenv("TRACK_ROOT")
-	if root == "" {
-		root, _ = os.Getwd()
-	}
-	root, _ = filepath.Abs(root)
-	inputChan := make(chan FileData)
-	outputChan := make(chan FileData)
-
-	csvFile, err := os.Create(
-		filepath.Join(root, "xam.csv"))
-	if err != nil {
-		panic(err)
-	}
-	defer csvFile.Close()
-
-	go writeCSV(outputChan, csvFile)
-
-	for _ = range iter.N(runtime.NumCPU()) {
-		go computeHashes(outputChan, inputChan)
-	}
-
-	walkFSTree(inputChan, root)
 }
